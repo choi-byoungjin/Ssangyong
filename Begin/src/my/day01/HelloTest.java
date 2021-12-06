c243b6b8994e6a4571b7cbf63df3ed467abc321d
choi-byoungjin <choibyoungjin85@gmail.com>
2021-12-06 14:32:18 +0900

    12월 06일 테스트

---
+++ Begin/src/my/day01/HelloTest.java
@@ -0,0 +1,18 @@
+package my.day01;
+
+//import java.lang.*;	항상 생략되어있음
+import java.util.Date;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("안녕하세요~~");
+		
+		Date now = new Date();
+		
+		System.out.println("현재시각 : " + now);
+	}
+
+}
