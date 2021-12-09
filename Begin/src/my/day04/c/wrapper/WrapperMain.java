746fcbe5be5de987fd794e0f0df8b2661b7b031d
choi-byoungjin <choibyoungjin85@gmail.com>
2021-12-09 17:01:50 +0900

    12월 09일 7교시

---
+++ Begin/src/my/day04/c/wrapper/WrapperMain.java
@@ -0,0 +1,30 @@
+package my.day04.c.wrapper;
+/*
+=== *** wrapper 클래스 *** ===
+
+------------------------------------------------
+  기본자료형(원시형, primitive)     wrapper 클래스
+------------------------------------------------
+ byte                           Byte 
+ short                          Short
+ int                            Integer  
+ long                           Long
+ char                           Character
+ float                          Float
+ double                         Double
+ boolean                        Boolean 
+ ------------------------------------------------
+ 
+  기본자료형(원시형)은 데이터 저장 및 4칙연산 에서만 사용하는 것이고, 
+ wrapper 클래스는  데이터 저장 및 4칙연산 뿐만아니라 
+  아주 다양한 기능의 메소드가 제공되므로 다방면으로 사용되어진다. 
+*/
+public class WrapperMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("=== auto Boxing 과 auto unBoxing 에 대해서 알아 봅니다. ===");
+
+	}
+
+}
