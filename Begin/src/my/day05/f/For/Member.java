06338ae50297bc7526e2bb27e116065250129a58
choi-byoungjin <choibyoungjin85@gmail.com>
2021-12-13 09:49:17 +0900

    12월 13일 (6일차)

--- Begin/src/my/day05/f/For/Member.java
+++ Begin/src/my/day05/f/For/Member.java
@@ -6,4 +6,11 @@ public class Member {
 	String passwd;
 	String name;
 	
+	void showInfo() {
+		System.out.println("\n=== 회원정보 ===\n"
+						+ "1. 아이디 : " + userid + "\n"
+						+ "2. 암호 : " + passwd + "\n"
+						+ "3. 성명 : " + name + "\n");
+	}
+	
 }
--- Begin/src/my/day05/f/For/MemberMain.java
+++ Begin/src/my/day05/f/For/MemberMain.java
@@ -10,19 +10,60 @@ public class MemberMain {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("=== 회원가입 하기 ===\n");
+		System.out.println("=== 회원가입 하기(유효성검사) ===\n");
+
 		
-	//	System.out.print("1. 아이디 => ");
-	//	String userid = sc.nextLine();
 		
-		System.out.print("2. 암호 => ");
-		String passwd = sc.nextLine();
+		String userid = "";
+		for(;;) {
+			System.out.print("1. 아이디 => ");
+			userid = sc.nextLine();
+			if(userid.length() == 0) {
+				System.out.println(">> [경고] 아이디는 필수 입력사항 입니다. << \n");
+			}
+			else {
+				break;
+			}
+		}// end of for --------------------------------------------------------
 		
-		// 암호는 8글자 이상 15글자 이하의 대문자, 소문자, 숫자, 특수문자가 혼합되어져야만 통과한다.
-		System.out.println(MyUtil.isCheckPasswd(passwd));
 		
-	//	System.out.print("3. 성명 => ");
-	//	String name = sc.nextLine();
+		
+		String passwd = "";
+		for(;;){
+			System.out.print("2. 암호 => ");
+			passwd = sc.nextLine();
+			
+			if(!MyUtil.isCheckPasswd(passwd)) {
+				System.out.println(">> [경고] 암호는 8글자 이상 15글자 이하의 대문자, 소문자, 숫자, 특수문자가 혼합되어져야만 합니다.<< \n");
+			}
+			else {
+				break;
+			}
+		}// end of for --------------------------------------------------------
+		
+		
+		
+		String name = "";
+		for(;;) {
+			System.out.print("3. 성명 => ");
+			name = sc.nextLine();
+			if(name.length() == 0) {
+				System.out.println(">> [경고] 성명은 필수 입력사항 입니다. << \n");
+			}
+			else {
+				break;
+			}
+		}// end of for --------------------------------------------------------
+		
+		
+
+		
+		Member mbr = new Member();
+		mbr.userid = userid;
+		mbr.passwd = passwd;
+		mbr.name = name;
+		
+		mbr.showInfo();
 		
 		sc.close();
 
