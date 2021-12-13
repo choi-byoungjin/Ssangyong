d97d7173c64e8ee36116a8ab39bb3d056a01c704
choi-byoungjin <choibyoungjin85@gmail.com>
2021-12-13 12:45:11 +0900

    12월 13일 4교시

--- Begin/src/my/day06/b/multiFor/gugudan/Gugudan_main1.java
+++ Begin/src/my/day06/b/multiFor/gugudan/Gugudan_main1.java
@@ -1,5 +1,7 @@
 package my.day06.b.multiFor.gugudan;
 
+import java.util.Scanner;
+
 public class Gugudan_main1 {
 
 	 /*
@@ -46,8 +48,62 @@ public class Gugudan_main1 {
 	 */
 	
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 		
+		/*
+		 * === 레이블(라벨)을 사용하여 break; 하기 ===
+		 *     레이블 뒤에는 : 을 붙이며 반드시 반복문 앞에 써야 한다.
+		 */
+		outer:	// outer: 을 레이블이라고 부르는데 그 레이블명이 지금은 outer이다. // 이름은 마음대로 짓는다.
+		for(;;) {
+			try {
+				System.out.print(">> 몇 단 볼래? => ");
+				String strDan = sc.nextLine();
+				
+				int dan = Integer.parseInt(strDan); // "1.34" 또는 "똘똘이"
+													// "345"
+				
+				if( 2 <= dan && dan <= 9) {
+					// 해당하는 단을 출력하기
+					
+					System.out.println("=== "+dan+"단 ===");
+					for (int i = 0; i < 9; i++) {
+						System.out.println(dan + "*" + (i+1) + "=" + (dan*(i+1)));
+					}
+					inner:
+					for(;;) {
+						System.out.print("\n >> 또 하시겠습니까?[Y/N] => ");
+						String yn = sc.nextLine();
+						
+					//	if("N".equals(yn) || "n".endsWith(yn)) { // yn <== "n" 또는 "N"
+					//	위와 같은 것이 아래이다.
+						if("N".equalsIgnoreCase(yn)) { // yn <== "n" 또는 "N"
+							sc.close();
+							break outer; // 레이블명이 outer 라고 선언되어진 반복문을 빠져나가는 것이다.
+						}
+						
+						else if("Y".equalsIgnoreCase(yn)) { // yn <== "y" 또는 "Y"
+							break;	// break; 는 가장 가까운 반복문(for)을 빠져나오는 것이다.
+						}
+						
+						else { // yn <== "y" 또는 "Y" 또는 "n" 또는 "N" 을 제외한 나머지 글자인 경우
+							System.out.println(">>> [경고] Y 또는 N 만 가능합니다!! <<< \n");
+						}
+						
+					} // end of for ---------------------------------------------------------
+					
+				}
+				else {
+					System.out.println(">>> [경고] 2단부터 9단까지만 가능합니다. <<< \n");
+				}
+				
+			} catch(NumberFormatException e) {
+				System.out.println(">>> [경고] 2단부터 9단까지만 가능합니다. <<< \n");
+			}
+		} // end of for --------------------------------------------------------
+		
+		System.out.println("\n== 프로그램종료 ==");
 
-	}
-
+	} // end of main ----------------------------------------------------------
 }
