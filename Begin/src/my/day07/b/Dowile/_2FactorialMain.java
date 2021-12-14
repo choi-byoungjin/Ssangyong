13aac781073729dec7fe915782754a78b0f81aa4
choi-byoungjin <choibyoungjin85@gmail.com>
2021-12-14 10:57:08 +0900

    12월 14일 2교시

--- Begin/src/my/day07/b/Dowile/_2FactorialMain.java
+++ Begin/src/my/day07/b/Dowile/_2FactorialMain.java
@@ -15,7 +15,8 @@ public class _2FactorialMain {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-
+		
+		outer :	// outer는 레이블이다.
 		do {
 			
 			try {
@@ -29,7 +30,7 @@ public class _2FactorialMain {
 					// continue; 를 만나면 아래로 떨어지지 않고 do~while(조건식); 의 조건식으로 들어간다.	// for문의 continue는 증감식
 				}
 				
-				int result = 1;
+				long result = 1;
 				for (int i = num; i > 0; i--) {
 					
 					result *= i;	// result = result * i;
@@ -41,18 +42,35 @@ public class _2FactorialMain {
 				}// end of for -----------------------------------------
 				
 				System.out.println(num+"! = "+result);
-				break;
+				
+				do {
+					System.out.print(">> 또 할래?[Y/N] => ");
+					String yn = sc.nextLine();
+					
+					if("Y".equalsIgnoreCase(yn)) {
+						break;	// 가장 가까운 반복문을 빠져나간다.
+					}
+					
+					else if("N".equalsIgnoreCase(yn)){
+						break outer;	// 레이블 지정된 바깥 반복문까지 빠져나간다.
+					}
+					else {	
+						System.out.println(">> [경고] Y 또는 N만 입력하세요!! \n");
+					}
+					
+				} while (true);
+				// end of do~while ---------------------------------------------
 				
 			} catch(NumberFormatException e) {
 				System.out.println(">> [경고] 정수만 입력하세요!! << \n");
 			}
 			
 		} while (true);
-		// end of do~while -------------------------------------------------
+		// end of do~while -----------------------------------------------------
 		
 		sc.close();
 		
 		System.out.println("\n~~~~~~~~~~~~~ 프로그램 종료 ~~~~~~~~~~~~~~~~");
-	}// end of main(String[] args)----------------------------------
+	}// end of main(String[] args) ---------------------------------------------
 
 }
