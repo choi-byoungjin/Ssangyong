0eed053e8a25ec452efdcd630b6cc6126dcd0657
choi-byoungjin <choibyoungjin85@gmail.com>
2021-12-16 09:49:17 +0900

    12월16일 1교시

--- Begin/src/my/day08/c/array/Member.java
+++ Begin/src/my/day08/c/array/Member.java
@@ -10,9 +10,16 @@ public class Member {
 	static int count;	// 다 같이 사용한다.
 	
 	// method
-	public String showInfo() {
+	// 특정사용자의 개인정보를 보여주도록 하는 메소드
+	public String showInfo(String id) {
 		return "1.아이디 : " + id + "\n"
 			+  "2.비밀번호 : " + passwd + "\n"
-			+  "3.성명 : " + name + "\n";
+			+  "3.성명 : " + name + "\n"
+			+  "4.포인트 : " + point;
+	}
+	
+	// 모든사용자의 비밀번호를 제외한 개인정보를 보여주도록 하는 메소드
+	public String showInfo() {
+		return id + "\t" + name + "\t" + point;
 	}
 }
--- Begin/src/my/day08/c/array/MemberMain.java
+++ Begin/src/my/day08/c/array/MemberMain.java
@@ -25,7 +25,7 @@ public class MemberMain {
 		do {
 		
 			System.out.println("\n========= >> 메뉴 << =========== \n"
-							+  "1.회원가입	 2.모든회원조회 3.프로그램종료\n"
+							+  "1.회원가입 2.모든회원조회 3.프로그램종료\n"
 							+  "=================================");
 			
 			System.out.print("▷ 선택하세요 => ");
@@ -42,68 +42,86 @@ public class MemberMain {
 				switch (menuNo) {
 					case 1:	// 회원가입
 						
-						Member mbr = new Member(); // 기본생성자
+						// 배열 mbrArr 에 빈방(비어있는 즉, null로 되어진 방)이 있는지 없는지를 확인한 후
+						// 빈방이 있으면 회원가입을 진행하고, 빈방이 없으면 정원마감 이라는 메시지를 출력하도록 한다.
 						
-						for(;;) {
-							System.out.print("\n1.아이디 : ");
-							String id = sc.nextLine();
-
-							if(id.length() == 0) {
-								System.out.println(">> [경고] 아이디는 필수입력 사항입니다. <<\n");
-							}
-							else {
-								mbr.id = id;
-								break;
-							}
-						}// end of for ----------------------------------------
-
-						for(;;) {
-							System.out.print("2.비밀번호 : ");
-							String passwd = sc.nextLine();
-
-							if(!(MyUtil.isCheckPasswd(passwd))) {
-								System.out.println(">> [경고] 비밀번호는 8글자 이상 15글자 이하에 대, 소문자, 숫자, 특수문자가 혼합되어져야만 합니다. <<\n");
-							}
-							else {
-								mbr.passwd = passwd;
-								break;
-							}
-						}// end of for ----------------------------------------
-						
-						for(;;) {
-							System.out.print("3.성명 : ");
-							String name = sc.nextLine();
+						if(Member.count < mbrArr.length) {
 							
-							if(name.length() == 0) {
-								System.out.println(">> [경고] 성명은 필수입력 사항입니다. <<\n");
-							}
-							else {
-								mbr.name = name;
-								break;
-							}
-						}// end of for ----------------------------------------
+							Member mbr = new Member(); // 기본생성자
+							
+							for(;;) {
+								System.out.print("\n1.아이디 : ");
+								String id = sc.nextLine();
+	
+								if( ) {	// 아이디 중복확인이 필요하다
+									System.out.println(">> [경고] 아이디가 중복되었습니다. 새로운 아이디를 입력하세요 <<\n");
+								}
+								else {
+									mbr.id = id;
+									break;
+								}
+							}// end of for ----------------------------------------
+	
+							for(;;) {
+								System.out.print("2.비밀번호 : ");
+								String passwd = sc.nextLine();
+	
+								if(!(MyUtil.isCheckPasswd(passwd))) {
+									System.out.println(">> [경고] 비밀번호는 8글자 이상 15글자 이하에 대, 소문자, 숫자, 특수문자가 혼합되어져야만 합니다. <<\n");
+								}
+								else {
+									mbr.passwd = passwd;
+									break;
+								}
+							}// end of for ----------------------------------------
+							
+							for(;;) {
+								System.out.print("3.성명 : ");
+								String name = sc.nextLine();
+								
+								if(name.length() == 0) {
+									System.out.println(">> [경고] 성명은 필수입력 사항입니다. <<\n");
+								}
+								else {
+									mbr.name = name;
+									break;
+								}
+							}// end of for ----------------------------------------
+							
+							for (int i = 0; i < mbrArr.length; i++) {
+								if(mbrArr[i] == null) {
+									mbrArr[i] = mbr;
+									break;
+								}
+							}// end of --------------------------------------------
+							/*
+							 * mbrArr[0] <== "이순신" 객체
+							 * mbrArr[1] <== "엄정화" 객체
+							 * mbrArr[2] <== 두명만 넣으면 null로 되어있음
+							 */
+							System.out.println(">> 회원가입 성공!! ["+(++Member.count)+" 번째 가입/정원 :"+mbrArr.length+" 명]<< \n");
+						} else {
+							System.out.println(">> [경고] 정원마감으로 인해 더 이상 회원가입이 불가합니다.");
+						}
 						
-						for (int i = 0; i < mbrArr.length; i++) {
-							if(mbrArr[i] == null) {
-								mbrArr[i] = mbr;
-								break;
-							}
-						}// end of --------------------------------------------
-						/*
-						 * mbrArr[0] <== "이순신" 객체
-						 * mbrArr[1] <== "엄정화" 객체
-						 * mbrArr[2] <== 두명만 넣으면 null로 되어있음
-						 */
-						System.out.println(">> 회원가입 성공!! ["+(++Member.count)+" 번째 가입/정원 :"+mbrArr.length+" 명]<< \n");
 						break; // switch의 break; 이다.
 						
 					case 2:	// 모든회원조회
 						
-						for (int i = 0; i < mbrArr.length; i++) {
-							System.out.println(mbrArr[i].showInfo()+ "\n");	// 멤버 객체 // showInfo 리턴타입 String
+						System.out.println("------------------------------------------");
+						System.out.println("아이디\t성명\t포인트");
+						System.out.println("------------------------------------------");
+						
+						String str_allMember_info = "";
+						for (int i = 0; i < Member.count; i++) {	// 가입한 인원수만큼만 반복한다.
+							str_allMember_info += mbrArr[i].showInfo()+ "\n";	// 멤버 객체 // showInfo 리턴타입 String이므로 계속 쌓아간다
+							
 						}// end of for ----------------------------------------
 						
+						System.out.println(str_allMember_info);
+						
 						break;
+						
 				}// end of switch (menuNo) ------------------------------------
 					
 			} catch(NumberFormatException e){
