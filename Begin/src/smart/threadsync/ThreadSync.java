ac88f5956273cae19a1fb98d2e8930e778d246e3
choi-byoungjin <choibyoungjin85@gmail.com>
2021-12-18 19:51:39 +0900

    스마트 혼합훈련 - 멀티스레드 이해하기

---
+++ Begin/src/smart/threadsync/ThreadSync.java
@@ -0,0 +1,49 @@
+package smart.threadsync;
+import java.util.*;
+
+public class ThreadSync {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Runnable r = new ThreadRun();
+		Thread t1 = new Thread(r);
+		Thread t2 = new Thread(r);
+				
+		System.out.println("[멀티 스레드 동기화 실습]");
+		t1.start();
+		t2.start();
+		
+		//new Thread(r).start();
+		//new Thread(r).start();
+		
+	}
+}
+class Counter {
+	private Integer count = 10;
+	public Integer getCount() {
+		return count;
+	}
+	public synchronized void incCount(String tname) {
+	//public void incCount(String tname) {
+		//synchronized(count) {
+			try {
+				Thread.sleep(10);
+			} catch(Exception e) { }
+			this.count++;
+			System.out.println(tname+": count increment : "+count);
+		//}
+	}
+}
+class ThreadRun implements Runnable {
+	Counter c = new Counter();
+
+	public void run() {
+		for(int i = 0; i < 10; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch(Exception e) { }
+			c.incCount(Thread.currentThread().getName());
+		}
+	}
+}
