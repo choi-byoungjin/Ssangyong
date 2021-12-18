dc12a5ab067fe06c4e5ada45c1c2983b6f278cce
choi-byoungjin <choibyoungjin85@gmail.com>
2021-12-18 16:21:21 +0900

    스마트 혼합훈련 - Object 클래스 실습

---
+++ Begin/src/smart/exim/Switch.java
@@ -0,0 +1,7 @@
+package smart.exim;
+
+public interface Switch  {
+	public static final String NAME = "[SWITCH]";
+	public void switchOn();
+	public void switchOff();
+}
---
+++ Begin/src/smart/objectclass/Robot.java
@@ -0,0 +1,38 @@
+package smart.objectclass;
+
+public class Robot extends Object {
+
+	int x;			
+	int y;			
+	int angle;		
+	int job;		
+	public Robot() {
+	}
+	public Robot(int x, int y, int angle, int job) {
+		this.x = x;
+		this.y = y;
+		this.angle = angle;
+		this.job = job;
+	}
+	
+	// toString() 오버라이딩
+	public String toString() {
+		String result ="[Robot]:";
+		result += "[X="+this.x+"]";
+		result += "[Y="+this.y+"]";
+		result += "[ANGLE="+this.angle+"]";
+		result += "[JOB="+this.job+"]";
+		return result;
+	}
+	
+	// equals() 오버라이딩
+	public boolean equals(Object obj) {
+		Robot r = (Robot)obj;	// 여기서 r은 b이다
+		if( r.x == x && r.y == y )
+		  	 return true;
+		else return false;
+	}
+	
+}
+
+
