d4aa2daa95d13209a0af9870e562e8e43526c0e5
choi-byoungjin <choibyoungjin85@gmail.com>
2021-12-20 15:51:02 +0900

    12월 20일 6교시

---
+++ Begin/src/my/day11/d/abstraction/Gujikja.java
@@ -0,0 +1,16 @@
+package my.day11.d.abstraction;
+
+public class Gujikja {
+	
+	// === 객체지향프로그래밍(Object Oriented Programming) 이란? === // 
+	// 어떤 프로그램이냐에 따라 그 목적에 맞도록 필요한 데이터(필드)를 추상화(필요한 것만 추출하는것. 반대가 정밀화)시키고,
+	// 또한 그 목적에 맞는 기능(메소드)만 부여한 클래스를 생성하여 프로그래밍하는 것이다.
+	
+	// === 객체지향프로그래밍(Object Oriented Programming) 의 특징 === //
+	/*
+	 	1. 상
+	 	2. 추
+	 	3. 캡
+	 	4. 다
+	 */
+}
