d3217cf5950d7c73d5c82cda0b28750b9c621c77
choi-byoungjin <choibyoungjin85@gmail.com>
2022-03-04 17:13:59 +0900

    3월 4일 JSP 1일차

---
+++ JSPServletBegin/src/main/java/chap02/GetMethod_01.java
@@ -0,0 +1,47 @@
+package chap02;
+
+import javax.servlet.http.*;
+
+public class GetMethod_01 extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+		System.out.println("~~~ 확인용 : doGet 메소드가 호출됨 ~~~");
+		
+		// HttpServletRequest request 객체는 전송되어져온 데이터를 처리해주는 용도로 쓰인다.
+		String name = request.getParameter("name");
+		String school= request.getParameter("school");
+		String color = request.getParameter("color");
+		
+		String[] arrFood = request.getParameterValues("food");
+		String food = String.join(",", arrFood);
+		
+		/*
+			// === 8. String.join("구분자", 문자열배열명) ===
+		    //        문자열배열을 "구분자"로 합쳐서 String 타입으로 돌려주는 것이다.
+		    String[] nameArr = {"한석규","두석규","세석규","네석규","오석규"};  
+		    String names = String.join("-", nameArr);
+		    System.out.println(names);
+		    // "한석규","두석규","세석규","네석규","오석규"
+	    */
+		
+		// *** 콘솔에 출력하기 시작 *** //
+		System.out.println("name => " + name);
+		System.out.println("school => " + school);
+		System.out.println("color => " + color);
+		System.out.println("food => " + food);
+					
+		// *** 콘솔에 출력하기 끝 *** //
+	}
+
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+			
+		System.out.println("### 확인용 : doPost 메소드가 호출됨 ~~~");
+		
+	}
+}
---
+++ JSPServletBegin/src/main/webapp/chap01/03_1_todayTime.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<%-- 필요한 클래스를 import 하려면 <%@ page %> page directive(페이지 지시어)를 사용하여 import를 한다. --%>
+<%@ page import="java.util.Date, java.text.SimpleDateFormat"%>
+
+<%
+	// *** 현재시각을 알아오기 *** //
+	Date now = new Date();	// 현재시각
+	
+	SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+    String today = sdformat.format(now);
+    
+    String currentTime = String.format("%tF %tT %tA %tZ", now, now, now, now); 
+    
+%>
+
+<%= currentTime %>
